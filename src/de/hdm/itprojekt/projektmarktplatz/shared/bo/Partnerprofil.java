5380d2d9e1d76dc6ae1ea399b7e839fea978d9fc
ersinbarut <ersinbarut@ersins-mbp-2.fritz.box>
2017-04-20 18:13:35 +0200

    Business Object bearbeitet

--- src/de/hdm/itprojekt/projektmarktplatz/shared/bo/Partnerprofil.java
+++ src/de/hdm/itprojekt/projektmarktplatz/shared/bo/Partnerprofil.java
@@ -1,5 +1,29 @@
 package de.hdm.itprojekt.projektmarktplatz.shared.bo;
 
+import java.util.Date;
+
 public class Partnerprofil {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Date erstelldatum;
+	private Date aenderungsdatum;
+	
+	public Partnerprofil (Date erstelldatum, Date aenderungsdatum){
+		this.aenderungsdatum = aenderungsdatum;
+		this.erstelldatum = erstelldatum;
+	}
+	public Date getErstelldatum (){
+		return erstelldatum;
+	}
+	public Date getAenderungsdatum (){
+		return aenderungsdatum;
+	}
+	public void setErstelldatum (Date erstelldatum){
+		erstelldatum = erstelldatum;
+	}
+	public void setAenderungsdatum (Date aenderungsdatum){
+		aenderungsdatum = aenderungsdatum;
+	}
 
 }

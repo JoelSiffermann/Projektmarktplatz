8f138d11835dfff4b651722b92ab1b6b482bb305
AyseGuelay <ag122@hdm-stuttgart.de>
2017-04-24 18:48:52 +0200

    Neue Interface.

---
+++ src/de/hdm/itprojekt/projektmarktplatz/shared/ProjektmarktplatzAdministrationAsync.java
@@ -0,0 +1,14 @@
+package de.hdm.itprojekt.projektmarktplatz.shared;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.itprojekt.projektmarktplatz.shared.bo.*;
+
+public interface ProjektmarktplatzAdministrationAsync {
+	
+	 void init(AsyncCallback<Void> callback);
+
+}

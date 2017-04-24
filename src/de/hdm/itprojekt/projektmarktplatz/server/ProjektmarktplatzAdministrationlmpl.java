cf65e1896bd750b20c0eb043c19c8578d610bbfd
AyseGuelay <ag122@hdm-stuttgart.de>
2017-04-24 18:48:52 +0200

    TEST

--- src/de/hdm/itprojekt/projektmarktplatz/server/ProjektmarktplatzAdministrationlmpl.java
+++ src/de/hdm/itprojekt/projektmarktplatz/server/ProjektmarktplatzAdministrationlmpl.java
@@ -1,5 +1,25 @@
 package de.hdm.itprojekt.projektmarktplatz.server;
 
-public class ProjektmarktplatzAdministrationlmpl {
+import java.util.ArrayList;
+import java.util.Vector;
 
+import de.hdm.itprojekt.projektmarktplatz.server.db.*;
+import de.hdm.itprojekt.projektmarktplatz.shared.*;
+import de.hdm.itprojekt.projektmarktplatz.shared.bo.*;
+import de.hdm.itprojekt.projektmarktplatz.shared.ProjektmarktplatzAdministration.*;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+@SuppressWarnings("serial")
+
+public class ProjektmarktplatzAdministrationlmpl extends RemoteServiceServlet
+/**implements ProjektmarktplatzAdministration*/ {
+	
+	
+	  public ProjektmarktplatzAdministrationlmpl() throws IllegalArgumentException {
+		  
+		  }
+	
+
+	
+	
 }
--- src/de/hdm/itprojekt/projektmarktplatz/server/ServersideSettings.java
+++ src/de/hdm/itprojekt/projektmarktplatz/server/ServersideSettings.java
@@ -1,5 +1,15 @@
 package de.hdm.itprojekt.projektmarktplatz.server;
 
-public class ServersideSettings {
+import java.util.logging.Logger;
+import de.hdm.itprojekt.projektmarktplatz.shared.CommonSettings;
 
+public class ServersideSettings extends CommonSettings {
+
+	
+	  private static final String LOGGER_NAME = "Projektmarktplatz Server ";
+	  private static final Logger log = Logger.getLogger(LOGGER_NAME);
+	  
+	  public static Logger getLogger() {
+		    return log;
+		  }
 }

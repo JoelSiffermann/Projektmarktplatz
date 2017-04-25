8de481052e8cec0fad6ee4dcc854f1bee83b19b6
ersinbarut <ersinbarut@localhost>
2017-04-25 16:09:55 +0200

    Attribute Projektmarktplatzadministration

--- src/de/hdm/itprojekt/projektmarktplatz/server/ProjektmarktplatzAdministrationlmpl.java
+++ src/de/hdm/itprojekt/projektmarktplatz/server/ProjektmarktplatzAdministrationlmpl.java
@@ -15,6 +15,34 @@ public class ProjektmarktplatzAdministrationlmpl extends RemoteServiceServlet
 /**implements ProjektmarktplatzAdministration*/ {
 	
 	
+	/*Referenz auf Eigenschaft*/
+	private EigenschaftMapper eMapper = null;
+	
+	/*Referenz auf Projektmarktplatz*/
+	private ProjektmarktplatzMapper pmMapper = null;
+	
+	/*Referenz auf Bewertung*/
+	private BewertungMapper bMapper = null;
+	
+	/*Referenz auf Bewerbung*/
+	private BewerbungMapper bwMapper = null;
+	
+	/*Referenz auf Ausschreibung*/
+	private AusschreibungMapper aMapper = null;
+	
+	/*Referenz auf Projekt*/
+	private ProjektMapper prjMapper = null;
+	
+	/*Referenz auf Projektbeteiligung*/
+	private ProjektbeteilitungMapper prbtMapper = null;
+	
+	/*Referenz auf Organisationseinheit*/
+	private OrganisationseinheitMapper orgMapper = null;
+	
+	/*Referenz auf Partnerprofil*/
+	private PartnerprofilMapper prtrMapper = null;
+	
+	
 	  public ProjektmarktplatzAdministrationlmpl() throws IllegalArgumentException {
 		  
 		  }
